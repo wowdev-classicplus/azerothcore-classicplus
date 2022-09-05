a964a1c163712b07fc78eb77cc2e0c31800c8689
Ethan <ethan.a.noble@gmail.com>
2022-09-06 04:14:23 +1000

    updating quest text to be better

--- data/sql/custom/db_world/Quest_Welcome.sql
+++ data/sql/custom/db_world/Quest_Welcome.sql
@@ -1,23 +1,26 @@
--- Quest_Template
+/* Quest_Template */
 DELETE FROM `quest_template` WHERE (`ID` = 26035);
 INSERT INTO `quest_template` (`ID`, `QuestType`, `QuestLevel`, `MinLevel`, `QuestSortID`, `QuestInfoID`, `SuggestedGroupNum`, `RequiredFactionId1`, `RequiredFactionId2`, `RequiredFactionValue1`, `RequiredFactionValue2`, `RewardNextQuest`, `RewardXPDifficulty`, `RewardMoney`, `RewardMoneyDifficulty`, `RewardBonusMoney`, `RewardDisplaySpell`, `RewardSpell`, `RewardHonor`, `RewardKillHonor`, `StartItem`, `Flags`, `RequiredPlayerKills`, `RewardItem1`, `RewardAmount1`, `RewardItem2`, `RewardAmount2`, `RewardItem3`, `RewardAmount3`, `RewardItem4`, `RewardAmount4`, `ItemDrop1`, `ItemDropQuantity1`, `ItemDrop2`, `ItemDropQuantity2`, `ItemDrop3`, `ItemDropQuantity3`, `ItemDrop4`, `ItemDropQuantity4`, `RewardChoiceItemID1`, `RewardChoiceItemQuantity1`, `RewardChoiceItemID2`, `RewardChoiceItemQuantity2`, `RewardChoiceItemID3`, `RewardChoiceItemQuantity3`, `RewardChoiceItemID4`, `RewardChoiceItemQuantity4`, `RewardChoiceItemID5`, `RewardChoiceItemQuantity5`, `RewardChoiceItemID6`, `RewardChoiceItemQuantity6`, `POIContinent`, `POIx`, `POIy`, `POIPriority`, `RewardTitle`, `RewardTalents`, `RewardArenaPoints`, `RewardFactionID1`, `RewardFactionValue1`, `RewardFactionOverride1`, `RewardFactionID2`, `RewardFactionValue2`, `RewardFactionOverride2`, `RewardFactionID3`, `RewardFactionValue3`, `RewardFactionOverride3`, `RewardFactionID4`, `RewardFactionValue4`, `RewardFactionOverride4`, `RewardFactionID5`, `RewardFactionValue5`, `RewardFactionOverride5`, `TimeAllowed`, `AllowableRaces`, `LogTitle`, `LogDescription`, `QuestDescription`, `AreaDescription`, `QuestCompletionLog`, `RequiredNpcOrGo1`, `RequiredNpcOrGo2`, `RequiredNpcOrGo3`, `RequiredNpcOrGo4`, `RequiredNpcOrGoCount1`, `RequiredNpcOrGoCount2`, `RequiredNpcOrGoCount3`, `RequiredNpcOrGoCount4`, `RequiredItemId1`, `RequiredItemId2`, `RequiredItemId3`, `RequiredItemId4`, `RequiredItemId5`, `RequiredItemId6`, `RequiredItemCount1`, `RequiredItemCount2`, `RequiredItemCount3`, `RequiredItemCount4`, `RequiredItemCount5`, `RequiredItemCount6`, `Unknown0`, `ObjectiveText1`, `ObjectiveText2`, `ObjectiveText3`, `ObjectiveText4`, `VerifiedBuild`) VALUES
 (26035, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 56807, 8, 0, 5740, 1, 9313, 1, 9315, 1, 9318, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Welcome to Classic Plus. (bf)', '', '
 ', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 'Objective text #1', 'Objective text #2', 'Objective text #3', 'Objective text #4', 0);
 
---Quest offer rewards
+/* Quest offer rewards*/
 DELETE FROM `quest_offer_reward` WHERE (`ID` = 26035);
 INSERT INTO `quest_offer_reward` (`ID`, `Emote1`, `Emote2`, `Emote3`, `Emote4`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `EmoteDelay4`, `RewardText`, `VerifiedBuild`) VALUES
-(26035, 4, 10, 0, 0, 0, 1500, 0, 0, 'Thank you for joining us to adventure in the land of Azeroth once more $N. We hope to bring you new and exciting original content and art, plus refreshing additions to old.$B$BOur goal with the server is to run Vanilla(1.12.1) content on the Wrath Engine (3.3.5), with new additions of our own. We wont get everything right, and we ask for your patience. If you want to help out - please feel free to comment in discord and tell us how the quests, items, mobs etc are innacurate to the 1.12.1 core experience. $B$BWith the formalities out of the way, thank you so much for joining us ', 0);
+(26035, 4, 10, 0, 0, 0, 1500, 0, 0, 'Thank you for joining us to adventure in the land of Azeroth once more $N. We hope to bring you new and exciting original content and art, plus refreshing additions to old.$B$B
 
--- Quest_Request_Items
+Our goal is to Classic content on the Wrath client, with new additions of our own. We ask for your patience, as we wont get everything right. Please feel free to comment in discord and tell us if any quests, items, mobs etc are innacurate to the base classic experience. 
+
+$B$BWith the formalities out of the way, good luck in the land of Azeroth.', 0);
+
+/* Quest Request Items */
 DELETE FROM `quest_request_items` WHERE (`ID` = 26035);
 INSERT INTO `quest_request_items` (`ID`, `EmoteOnComplete`, `EmoteOnIncomplete`, `CompletionText`, `VerifiedBuild`) VALUES
 (26035, 4, 0, '', 0);
 
--- Creature QuestStarter
+/* Creature Quest Starter */
 DELETE FROM `creature_queststarter` WHERE (`quest` = 26035);
 INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
-(0, 26035),
 (658, 26035),
 (823, 26035),
 (1568, 26035),
@@ -25,10 +28,9 @@ INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
 (2980, 26035),
 (10176, 26035);
 
--- Creature Questender
+/* Creature Quest Ender */
 DELETE FROM `creature_questender` WHERE (`quest` = 26035);
 INSERT INTO `creature_questender` (`id`, `quest`) VALUES
-(0, 26035),
 (658, 26035),
 (823, 26035),
 (1568, 26035),
