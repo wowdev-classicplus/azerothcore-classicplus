fe672ab5c10ff9c445f1bb6b745ac88b7362b0f4
javi <javier.fernandez15@gmail.com>
2022-09-09 11:04:18 +1000

    Cleaned up custom SQL folder

---
+++ data/sql/custom/db_world/dbdiff/REPLACE_gameobject_template_addon_2022-09-09_10_47.sql
@@ -0,0 +1,9 @@
+
+REPLACE INTO acore_world.gameobject_template_addon(entry, faction, flags, mingold, maxgold, artkit0, artkit1, artkit2, artkit3)
+VALUES (401009, 0, 0, 0, 0, 0, 0, 0, 0)
+      ,(401010, 0, 0, 0, 0, 0, 0, 0, 0)
+      ,(401011, 0, 0, 0, 0, 0, 0, 0, 0)
+      ,(401012, 0, 0, 0, 0, 0, 0, 0, 0)
+      ,(401013, 0, 0, 0, 0, 0, 0, 0, 0)
+;
+
---
+++ data/sql/custom/db_world/dbdiff/REPLACE_quest_request_items_2022-09-09_10_47.sql
@@ -0,0 +1,6 @@
+
+REPLACE INTO acore_world.quest_request_items(ID, EmoteOnComplete, EmoteOnIncomplete, CompletionText, VerifiedBuild)
+VALUES (26035, 4, 0, N'', 0)
+      ,(26037, 0, 0, N'', 0)
+;
+
